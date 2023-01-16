8107fb7d13ec6338216352dc1d616c0922cc70b2
MostafaGamal <motafa.gamal@codzone-eg.com>
2023-01-16 17:26:22 +0200

    added custom permissions class and check permission for global checking a permission

---
+++ QuickNote/Helpers/CustomPermission.cs
@@ -0,0 +1,16 @@
+﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace QuickNote.Helpers
+{
+    internal class NotificationPermission : BasePlatformPermission
+    {
+#if ANDROID
+        
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
+            new List<(string permission, bool isRuntime)>
+            {
+            ("android.permission.POST_NOTIFICATIONS", true)
+            }.ToArray();
+#endif    
+    }
+}
